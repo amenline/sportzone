04b12297063682902cc37843034ae5c09ac4687e
Ron Mens <amenline.07@gmail.com>
2021-05-31 06:25:04 +0000

    added images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,26 @@
 // import Head from 'next/head'
 
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
 
 const Home: React.FC = () => {
   return (
-    <Layout title="Sportzone - Home">
+    <Layout title='Sportzone - Home'>
       <h1>This is Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora et est
-        voluptatem veritatis vero ipsa minima at eaque, ab ipsam tenetur
-        accusamus, eligendi porro magni natus aut doloremque! Ducimus, quas?
-      </p>
+      <section className='w-full h-96 bg-gray-700'>
+        <div className='flex'>
+          <div>
+            <img src='/images/1.jpg' alt='first image' />
+          </div>
+          <div>
+            <img src='/images/2.jpg' alt='second image' />
+          </div>
+          <div>
+            <img src='/images/3.png' alt='third image' />
+          </div>
+        </div>
+      </section>
+      <div className='round teams'></div>
+      <section></section>
     </Layout>
   );
 };
